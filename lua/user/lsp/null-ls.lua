3ca7fbdcdea2b04151dfbabfc47ddb3c93f4e4a4
bruce <cnhxunoo@gmail.com>
2022-06-22 18:53:09 +0800

    fixed null yapf

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -16,9 +16,10 @@ null_ls.setup {
       extra_filetypes = { "toml" },
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
-    formatting.black.with { extra_args = { "--fast" } },
-    formatting.stylua,
-    formatting.google_java_format,
+    -- formatting.black.with { extra_args = { "--fast" } },
+    -- formatting.stylua,
+    -- formatting.google_java_format,
+    formatting.yapf,
     diagnostics.flake8,
   },
 }
