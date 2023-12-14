e467fd4e83355b68b435da3af69585a5dc45d290
bruce <cnhxunoo@gmail.com>
2023-12-14 10:19:47 +0800

    not use single-quote in prettier

--- lua/config/lsp/null-ls.lua
+++ lua/config/lsp/null-ls.lua
@@ -11,7 +11,7 @@ null_ls.setup {
   sources = {
     formatting.prettier.with {
       extra_filetypes = { "toml", "html", "htmldjango", "javascript", "typescript", "jsx", "tsx", "markdown", "css", "json" , "rs"},
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+      -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
     -- formatting.black.with { extra_args = { "--fast" } },
 		 --   diagnostics.flake8,
