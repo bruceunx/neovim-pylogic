9463049883e1a324a52fad155507f74a26cc3bc9
bruce <cnhxunoo@gmail.com>
2024-02-23 16:54:52 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Based on Neovim from scratch
+# Neovim with Old config and lazyvim
 
+- use lazyvim plug as a plugin manager
+
+- adjust old config with lazyvim
+
+- support C, CPP, C#, Python, Rust, CSS, JS, TS
+
+- support DAP for c, cpp, python, rust and c#
+
+- transform null-ls to nono-ls with lazyvim
--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -18,54 +18,54 @@ dap_install.setup({})
 dap_install.config("python", {})
 
 dap.adapters.python = function(cb, config)
-  if config.request == 'attach' then
-    ---@diagnostic disable-next-line: undefined-field
-    local port = (config.connect or config).port
-    ---@diagnostic disable-next-line: undefined-field
-    local host = (config.connect or config).host or '127.0.0.1'
-    cb({
-      type = 'server',
-      port = assert(port, '`connect.port` is required for a python `attach` configuration'),
-      host = host,
-      options = {
-        source_filetype = 'python',
-      },
-    })
-  else
-    cb({
-      type = 'executable',
-      command = '/opt/homebrew/Caskroom/miniconda/base/envs/ps/bin/python',
-      args = { '-m', 'debugpy.adapter' },
-      options = {
-        source_filetype = 'python',
-      },
-    })
-  end
+	if config.request == "attach" then
+		---@diagnostic disable-next-line: undefined-field
+		local port = (config.connect or config).port
+		---@diagnostic disable-next-line: undefined-field
+		local host = (config.connect or config).host or "127.0.0.1"
+		cb({
+			type = "server",
+			port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+			host = host,
+			options = {
+				source_filetype = "python",
+			},
+		})
+	else
+		cb({
+			type = "executable",
+			command = "/opt/homebrew/Caskroom/miniconda/base/envs/ps/bin/python",
+			args = { "-m", "debugpy.adapter" },
+			options = {
+				source_filetype = "python",
+			},
+		})
+	end
 end
 
 dap.configurations.python = {
-  {
-    type = 'python';
-    request = 'launch';
-    name = "Launch file";
-    program = "${file}";
-    pythonPath = function()
-      local cwd = vim.fn.getcwd()
-      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-        return cwd .. '/venv/bin/python'
-      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
-        return cwd .. '/.venv/bin/python'
-      else
-        return '/opt/homebrew/Caskroom/miniconda/base/envs/ps/bin/python'
-      end
-    end;
-  },
+	{
+		type = "python",
+		request = "launch",
+		name = "Launch file",
+		program = "${file}",
+		pythonPath = function()
+			local cwd = vim.fn.getcwd()
+			if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+				return cwd .. "/venv/bin/python"
+			elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+				return cwd .. "/.venv/bin/python"
+			else
+				return "/opt/homebrew/Caskroom/miniconda/base/envs/ps/bin/python"
+			end
+		end,
+	},
 }
 
 dap.adapters.lldb = {
-  type = 'executable',
-  command = '/usr/bin/lldb-vscode',
-  name = 'lldb'
+	type = "executable",
+	command = "/usr/bin/lldb-vscode",
+	name = "lldb",
 }
 
 local install_root_dir = vim.fn.stdpath("data") .. "/mason"
@@ -84,17 +84,12 @@ dap.adapters.codelldb = {
 	},
 }
 
-
 local lldb = {
 	name = "Launch lldb",
 	type = "lldb", --lldb or codelldb
 	request = "launch",
 	program = function()
-		return vim.fn.input(
-			"Path to executable: ",
-			vim.fn.getcwd() .. "/",
-			"file"
-		)
+		return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 	end,
 	cwd = "${workspaceFolder}",
 	stopOnEntry = false,
@@ -103,30 +98,28 @@ local lldb = {
 }
 
 dap.configurations.cpp = {
-	lldb
+	lldb,
 }
 
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
 
 dap.adapters.coreclr = {
-  type = 'executable',
-  command = '/Users/bruce/.local/share/nvim/mason/packages/netcoredbg/netcoredbg',
-  args = {'--interpreter=vscode'}
+	type = "executable",
+	command = "/Users/bruce/.local/share/nvim/mason/packages/netcoredbg/netcoredbg",
+	args = { "--interpreter=vscode" },
 }
 
 dap.configurations.cs = {
-  {
-    type = "coreclr",
-    name = "launch - netcoredbg",
-    request = "launch",
-    program = function()
-        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/net7.0/', 'file')
-    end,
-  },
+	{
+		type = "coreclr",
+		name = "launch - netcoredbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/net8.0/", "file")
+		end,
+	},
 }
-
-
 
 dapui.setup({
 	expand_lines = true,
