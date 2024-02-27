b925098e7372ed1e895f75b2b8307c74a41043ad
bruce <cnhxunoo@gmail.com>
2024-02-27 18:02:03 +0800

    add prettier to null-ls config sources

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -6,6 +6,7 @@ return {
 		opts.root_dir = opts.root_dir
 			or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
 		opts.sources = vim.list_extend(opts.sources or {}, {
+			nls.builtins.formatting.prettier,
 			nls.builtins.formatting.ruff,
 			nls.builtins.formatting.yapf,
 			nls.builtins.diagnostics.ruff,
