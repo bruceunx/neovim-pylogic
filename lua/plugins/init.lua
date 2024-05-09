90bb4d6b1438217985fddc0ead52b800cd46a7e1
bruce <cnhxunoo@gmail.com>
2024-05-09 08:23:53 +0800

    :pencil2: add support qml syntax highlight

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,19 @@
 return {
-{ 'AndreM222/copilot-lualine' },
- { "famiu/bufdelete.nvim" },
- -- { "voldikss/vim-floaterm"},
- { "iamcco/markdown-preview.nvim", build = "cd app && npm install", init = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, },
- { "Hoffs/omnisharp-extended-lsp.nvim", lazy=true},
+	{ "AndreM222/copilot-lualine" },
+	{ "famiu/bufdelete.nvim" },
+	-- { "voldikss/vim-floaterm"},
+	{
+		"iamcco/markdown-preview.nvim",
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	{ "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
+	{
+		"peterhoeg/vim-qml",
+		event = "BufRead",
+		ft = { "qml" },
+	},
 }
